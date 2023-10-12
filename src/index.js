ac9b308262c3b94d2290f4558ec1540913d20a0b
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-10-12 02:01:10 +0200

    fix: simpleLightBox

--- src/index.js
+++ src/index.js
@@ -41,22 +41,19 @@ const status ={
   },
 };
 
-document.querySelector('.gallery');
-document.querySelector('.search-form').setAttribute('Style','background-color: green; padding: 10px 0;');
-document.querySelector('.load-more').setAttribute('Style', 'padding: 20px 40px; background-color: green;color: white;display:none;')
+// document.querySelector('.gallery');
+// document.querySelector('.search-form').setAttribute('Style','background-color: green; padding: 10px 0;');
+document.querySelector('.load-more').setAttribute('Style', 'display:none;')
 
 
 
 function render(hits) {
 
-  // clear();
-  const fragment = document.createDocumentFragment();
-  const gallery = document.createElement('div')
-  gallery.setAttribute('class', 'gallery');
+const gallery = document.querySelector('.gallery');
 
-hits.forEach((item) => {
+const markupCard = hits.map((item) => {
 
-      const markupCard = `<div class="photo-card">
+      return  `<div class="photo-card">
                            <a class="gallery__item" href="${item.webformatURL}">
                              <img class="gallery__image" src="${item.webformatURL}" alt="" loading="lazy" width="450px"; height="450px"/><a/>
                              <div class="info">
@@ -80,61 +77,47 @@ hits.forEach((item) => {
 
                           </div>`
 
-  gallery.insertAdjacentHTML('beforeend',markupCard);
-  });
+  // return markup;
 
+}).join('');
 
-gallery.querySelectorAll('.info-item-value').forEach((item) => {
-    item.setAttribute('Style', 'display: block;')
-  });
+gallery.innerHTML = markupCard;
 
-  gallery.querySelectorAll('.info-item').forEach((item) => {
+// gallery.querySelectorAll('.info-item-value').forEach((item) => {
+//     item.setAttribute('Style', 'display: block;')
+//   });
 
-    item.setAttribute('Style', 'margin: 20px; border: ');
-
-});
-
-  gallery.querySelectorAll('.info').forEach((item) => {
-    item.setAttribute('Style', 'display:flex; justify-content: space-evenly;')
-  });
-
-  gallery.querySelectorAll('.photo-card').forEach((card) => {
-      card.setAttribute(
-        'Style',
-         'box-shadow: 0 3px 10px 0 #aaa; cursor: pointer; height: 550px; width: 460px;'
-      )
-  });
-
-  gallery.querySelectorAll('img').forEach((image) => {
-      image.setAttribute('Style', 'padding: 5px;')
-  });
-
-  gallery.querySelectorAll('.search-form').forEach((item) => {
-      item.setAttribute('Style', 'box-shadow: 0 3px 10px 0 #aaa');
-  });
-
-  document.querySelector('.load-more').setAttribute('Style', 'padding: 20px 40px; background-color: green;color: white;display:inline-blocks;')
-
-  fragment.append(gallery);
-  fragment.querySelector('.gallery').setAttribute('Style', 'display: flex; flex-wrap: wrap; gap: 20px 20px;margin:10px auto 20px auto;');
-//
-  const box = document.querySelector('.gallery');
-  // if (!box.hasChildNodes()) {
-  //   box.appendChild(fragment);
-  // }else {
-  //   box.replaceWith(fragment);
-  // };
-  box.replaceWith(fragment);
-
-
-// const gallery_1 = new SimpleLightbox('.gallery a', {
-//     overlay: true,
-//     overlayOpacity: 0.7,
-//     spinner: true,
-//     nav: true,
-//     close: true,
-//     loop: true,
+// gallery.querySelectorAll('.info-item').forEach((item) => {
+//     item.setAttribute('Style', 'margin: 20px; border: ');
 // });
+
+ // gallery.querySelectorAll('.info').forEach((item) => {
+ //   item.setAttribute('Style', 'display:flex; justify-content: space-evenly;')
+ // });
+
+// gallery.querySelectorAll('.photo-card').forEach((card) => {
+//     card.setAttribute(
+//       'Style',
+//        'box-shadow: 0 3px 10px 0 #aaa; cursor: pointer; height: 550px; width: 460px;'
+//     )
+// });
+
+// gallery.querySelectorAll('img').forEach((image) => {
+//       image.setAttribute('Style', 'padding: 5px;')
+//   });
+
+// gallery.querySelectorAll('.search-form').forEach((item) => {
+//      item.setAttribute('Style', 'box-shadow: 0 3px 10px 0 #aaa');
+//  });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// gallery.setAttribute('Style', 'display: flex; flex-wrap: wrap; gap: 20px 20px;margin:10px auto 20px auto;');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+ document.querySelector('.load-more').setAttribute('Style', 'display:inline-blocks;');
 
 const gallery_1 = new SimpleLightbox('.gallery a', {
     maxZoom:3,
@@ -144,18 +127,139 @@ const gallery_1 = new SimpleLightbox('.gallery a', {
   });
 
 
-gallery_1.on('changed.simplelightbox', function () {
-	const { height: cardHeight } = document
-  .querySelector(".gallery")
-  .firstElementChild.getBoundingClientRect();
+// const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
+//
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: "smooth",
+// });
 
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: "smooth",
-});
-});
+gallery_1.refresh();
 
 }
+
+
+// function render_1(hits) {
+//
+//   // clear();
+//   let markupCard='';
+//   const fragment = document.createDocumentFragment();
+//   const gallery = document.createElement('div')
+//   gallery.setAttribute('class', 'gallery');
+//
+// hits.forEach((item) => {
+//
+//       markupCard = `<div class="photo-card">
+//                            <a class="gallery__item" href="${item.webformatURL}">
+//                              <img class="gallery__image" src="${item.webformatURL}" alt="" loading="lazy" width="450px"; height="450px"/><a/>
+//                              <div class="info">
+//                                <p class="info-item">
+//                                  <b>Likes</b>
+//                                  <span class="info-item-value" >${item.likes}</span>
+//                                </p>
+//                                <p class="info-item">
+//                                  <b>Views</b><br>
+//                                  ${item.views}
+//                                </p>
+//                                <p class="info-item">
+//                                  <b>Comments</b><br>
+//                                  ${item.comments}
+//                                </p>
+//                                <p class="info-item">
+//                                  <b>Downloads</b><br>
+//                                  ${item.downloads}
+//                                </p>
+//                              </div>
+//
+//                           </div>`
+//
+//   gallery.insertAdjacentHTML('beforeend',markupCard);
+//   });
+//
+//
+// gallery.querySelectorAll('.info-item-value').forEach((item) => {
+//     item.setAttribute('Style', 'display: block;')
+//   });
+//
+//   gallery.querySelectorAll('.info-item').forEach((item) => {
+//
+//     item.setAttribute('Style', 'margin: 20px; border: ');
+//
+// });
+//
+//   gallery.querySelectorAll('.info').forEach((item) => {
+//     item.setAttribute('Style', 'display:flex; justify-content: space-evenly;')
+//   });
+//
+//   gallery.querySelectorAll('.photo-card').forEach((card) => {
+//       card.setAttribute(
+//         'Style',
+//          'box-shadow: 0 3px 10px 0 #aaa; cursor: pointer; height: 550px; width: 460px;'
+//       )
+//   });
+//
+//   gallery.querySelectorAll('img').forEach((image) => {
+//       image.setAttribute('Style', 'padding: 5px;')
+//   });
+//
+//   gallery.querySelectorAll('.search-form').forEach((item) => {
+//       item.setAttribute('Style', 'box-shadow: 0 3px 10px 0 #aaa');
+//   });
+//
+//   document.querySelector('.load-more').setAttribute('Style', 'padding: 20px 40px; background-color: green;color: white;display:inline-blocks;')
+//
+//   fragment.append(gallery);
+//   fragment.querySelector('.gallery').setAttribute('Style', 'display: flex; flex-wrap: wrap; gap: 20px 20px;margin:10px auto 20px auto;');
+// //
+//   const box = document.querySelector('.gallery');
+//   // if (!box.hasChildNodes()) {
+//   //   box.appendChild(fragment);
+//   // }else {
+//   //   box.replaceWith(fragment);
+//   // };
+//
+//
+// // if (status.getPage() === 1) {
+// //     box.replaceWith(fragment);
+// //   }else {
+// //     box.insertAdjacentHTML('beforeend', markupCard);
+// //   };
+//
+//     box.replaceWith(fragment);
+//
+// // const gallery_1 = new SimpleLightbox('.gallery a', {
+// //     overlay: true,
+// //     overlayOpacity: 0.7,
+// //     spinner: true,
+// //     nav: true,
+// //     close: true,
+// //     loop: true,
+// // });
+//
+// const gallery_1 = new SimpleLightbox('.gallery a', {
+//     maxZoom:3,
+//     widthRatio: 0.8,
+//     heightRatio: 0.9,
+//     scaleImageToRatio: true,
+//   });
+//
+//
+// gallery_1.on('changed.simplelightbox', function () {
+// 	const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
+//
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: "smooth",
+// });
+// });
+//
+// gallery_1.refresh();
+//
+// }
 
 const msgForm = document.querySelector('.search-form');
 
@@ -176,15 +280,7 @@ msgForm.addEventListener("submit", (e) => {
         const total = value.total;
         if (total !== 0) {
           const hits = value.hits;
-          const total = value.total;
           const totalHits = value.totalHits;
-          const webformatURL = value.hits[0].webformatURL;
-          const largeImageURL = value.hits[0].largeImageURL;
-          const tags = value.hits[0].tags;
-          const likes = value.hits[0].likes;
-          const views = value.hits[0].views;
-          const comments = value.hits[0].comments;
-          const downloads = value.hits[0].downloads;
           status.setTotalHits(totalHits);
           console.log('TOTAL PAGE: ', status.tatalPage);
           console.log('PAGE: ',status.page)
@@ -211,15 +307,6 @@ btn.addEventListener('click', () => {
       const total = value.total;
       if (total !== 0) {
         const hits = value.hits;
-        const total = value.total;
-        const totalHits = value.totalHits;
-        const webformatURL = value.hits[0].webformatURL;
-        const largeImageURL = value.hits[0].largeImageURL;
-        const tags = value.hits[0].tags;
-        const likes = value.hits[0].likes;
-        const views = value.hits[0].views;
-        const comments = value.hits[0].comments;
-        const downloads = value.hits[0].downloads;
         console.log('TOTAL PAGE: ', status.tatalPage);
         console.log('PAGE: ', status.page);
         render(hits);
